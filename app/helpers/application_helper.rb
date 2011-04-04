58c27aa3b475d12140cd45ed6c3bb906e5c53611
Freddie <freddieboi@gmail.com>
2011-04-05 00:16:58 +0200

    Added initial Microposts and support for facebook like and twitter tweet buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,24 @@
 module ApplicationHelper
-  # Return a title on a per-page basis.
-  def title
-    base_title = "RailsTest"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-  end
-  
-  def logo
-  	  image_tag("logo.png", :alt => "Sample App", :class => "round")
-  end
+	include TweetButton
+	# Return a title on a per-page basis.
+	def title
+		base_title = "RailsTest"
+		if @title.nil?
+		base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
+	def logo
+		image_tag("logo.png", :alt => "RailsTest", :class => "round")
+	end
+
+	def facebook_like
+		src = "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80"
+		content_tag :iframe, nil, :src => src, :scrolling => 'no', :frameborder => '0',
+						:allowtransparency => true, :id => :facebook_like
+	end
+
+	TweetButton.default_tweet_button_options = {:via => "myself", :count => "horizontal"}
 end
