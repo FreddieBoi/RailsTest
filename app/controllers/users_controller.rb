3fdc7fd177b10b61719bffcba4b45bf3ff23f12e
Freddie <freddieboi@gmail.com>
2011-04-04 17:41:05 +0200

    Now listing all users with paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-	before_filter :authenticate, :only => [:edit, :update]
+	before_filter :authenticate, :only => [:index, :edit, :update]
 	before_filter :correct_user, :only => [:edit, :update]
 	
+	def index
+		@title = "All users"
+		@users = User.paginate(:page => params[:page])
+	end
+
 	def show
 		@user = User.find(params[:id])
 		@title = @user.name
@@ -40,13 +45,13 @@ class UsersController < ApplicationController
 
 	private
 
-		    def authenticate
-		      deny_access unless signed_in?
-		    end
-		
-		    def correct_user
-		      @user = User.find(params[:id])
-		      redirect_to(root_path) unless current_user?(@user)
-		    end
+		def authenticate
+			deny_access unless signed_in?
+		end
+	
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
 
 end
