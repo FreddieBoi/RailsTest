8aacaad898bca459a5fd2858762f3b795f10df39
Freddie <freddieboi@gmail.com>
2011-04-06 01:15:56 +0200

    Made it possible to follow/unfollow other Users through Relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110403233844
+# Schema version: 20110405102322
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
@@ -17,6 +18,14 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 
 	has_many :microposts, :dependent => :destroy
+
+	has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+	has_many :following, :through => :relationships, :source => :followed
+	has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+	has_many :followers, :through => :reverse_relationships, :source => :follower
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -47,27 +56,39 @@ class User < ActiveRecord::Base
 		(user && user.salt == cookie_salt) ? user : nil
 	end
 
+	def following?(followed)
+		relationships.find_by_followed_id(followed)
+	end
+
+	def follow!(followed)
+		relationships.create!(:followed_id => followed.id)
+	end
+
+	def unfollow!(followed)
+		relationships.find_by_followed_id(followed).destroy
+	end
+
 	def feed
 		# Crappy implementation, will improve later...
-		microposts
+		Micropost.where("user_id = ?", id)
 	end
 
 	private
 
-		def encrypt_password
-			self.salt = make_salt if new_record?
-			self.encrypted_password = encrypt(password)
-		end
-	
-		def encrypt(string)
-			secure_hash("#{salt}--#{string}")
-		end
-	
-		def make_salt
-			secure_hash("#{Time.now.utc}--#{password}")
-		end
-	
-		def secure_hash(string)
-			Digest::SHA2.hexdigest(string)
-		end
+	def encrypt_password
+		self.salt = make_salt if new_record?
+		self.encrypted_password = encrypt(password)
+	end
+
+	def encrypt(string)
+		secure_hash("#{salt}--#{string}")
+	end
+
+	def make_salt
+		secure_hash("#{Time.now.utc}--#{password}")
+	end
+
+	def secure_hash(string)
+		Digest::SHA2.hexdigest(string)
+	end
 end
