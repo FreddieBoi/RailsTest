23608a9a85bfbf29ac59e532cc1718151dc68c23
Freddie <freddieboi@gmail.com>
2011-04-06 01:24:02 +0200

    Now following User posts are enlisted in Home feed as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,8 +69,7 @@ class User < ActiveRecord::Base
 	end
 
 	def feed
-		# Crappy implementation, will improve later...
-		Micropost.where("user_id = ?", id)
+		Micropost.from_users_followed_by(self)
 	end
 
 	private
